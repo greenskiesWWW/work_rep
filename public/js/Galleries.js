95a484b3330e491d93c075027114d324882cd53c
xoheveras <57674283+xoheveras@users.noreply.github.com>
2023-02-10 17:57:04 +0300

    add js

---
+++ public/js/Galleries.js
@@ -0,0 +1,24 @@
+var ids = document.querySelectorAll('[id]');
+
+Array.prototype.forEach.call( ids, function( el, i ) {
+
+    if(el.id == "galleries_js")
+    {
+        el.onclick = function () {
+            openModalGalleries(el.getAttribute("g_text"));
+        }
+    }
+});
+
+function openModalGalleries(value)
+{
+    let galleries_modal = document.getElementById('galleries_modal'); 
+    let caption_galleries = document.getElementById('caption_galleries'); 
+    let close_gallereies_modal = document.getElementById('close_gallereies_modal');
+
+    galleries_modal.style.display = "flex";
+    caption_galleries.innerHTML = "Galleries: " + value;
+    close_gallereies_modal.onclick = function() {
+        galleries_modal.style.display = "none";
+    }
+}
