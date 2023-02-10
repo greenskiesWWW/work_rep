3153a99b29537366e9c9e3c5f56c284975a34456
xoheveras <57674283+xoheveras@users.noreply.github.com>
2023-02-10 19:12:13 +0300

    js swapper

--- public/js/Galleries.js
+++ public/js/Galleries.js
@@ -5,23 +5,45 @@ Array.prototype.forEach.call( ids, function( el, i ) {
     if(el.id == "galleries_js")
     {
         el.onclick = function () {
-            openModalGalleries(el.getAttribute("g_text"));
+            openModalGalleries(el.getAttribute("target"), el.getAttribute("ctarget"));
+        }
+    }
+
+    if(el.id == "img-card-clicker")
+    {
+        el.onclick = function () {
+            changeImage(el, el.getAttribute("target"));
         }
     }
 });
 
-function openModalGalleries(value)
+function changeImage(el, target)
 {
-    let galleries_modal = document.getElementById('galleries_modal'); 
-    let caption_galleries = document.getElementById('caption_galleries'); 
-    let close_gallereies_modal = document.getElementById('close_gallereies_modal');
+
+    let img_target = document.getElementById("img-card-" + target); 
+
+    let target_temp = img_target.src;
+    let temp = el.src;
+
+    el.src = target_temp;
+    img_target.src = temp;
+
+}
+
+function openModalGalleries(target, ctarget)
+{
+    let galleries_modal = document.getElementById(target); 
+    let close_gallereies_modal = document.getElementById(ctarget);
+
+    try { galleries_modal.classList.remove('Anim-block-hide'); } catch (error) {}
 
     galleries_modal.classList.remove('Anim-block-hide');
     galleries_modal.classList.add('Anim-block-show');
     galleries_modal.style.display = "flex";
-    caption_galleries.innerHTML = "Galleries: " + value;
     close_gallereies_modal.onclick = function() {
-        galleries_modal.classList.remove('Anim-block-show');
+
+        try { galleries_modal.classList.remove('Anim-block-show'); } catch (error) {}
+
         galleries_modal.classList.add('Anim-block-hide');
         setTimeout(function() {
             galleries_modal.style.display = "none";
