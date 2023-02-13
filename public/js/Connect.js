f55c73c2738fb8f917eb2c5fd7047b04f7994f8f
xoheveras <57674283+xoheveras@users.noreply.github.com>
2023-02-13 16:14:54 +0300

    web3 change

--- public/js/Connect.js
+++ public/js/Connect.js
@@ -1,6 +1,30 @@
 // var web3;
 
 const walletID = document.getElementById("address");
+let account;
+let balance;
+
+
+async function getBalance()
+{
+    ethereum
+    .request({ method: "eth_getBalance", params: [account, "latest"]})
+    .then((balance) => {
+
+        console.log(balance)
+
+        balance = parseInt(balance, 16) / Math.pow(10, 18);;
+            walletID.innerHTML = "<span class='eth-adress'>"+balance+"</span>";
+
+    }).catch((error) => {
+        // Handle error
+        console.log(error, error.code);
+
+        // 4001 - The request was rejected by the user
+        // -32602 - The parameters were invalid
+        // -32603- Internal error
+    });
+}
 
 async function Connect(){
 
@@ -11,8 +35,9 @@ async function Connect(){
         .request({ method: "eth_requestAccounts" })
         .then((accounts) => {
 
-          const account = accounts[0]
+          account = accounts[0];
                 walletID.innerHTML = "<span class='eth-adress'>"+account.substring(0,23)+"</span>";
+                getBalance();
 
         }).catch((error) => {
             // Handle error
@@ -22,22 +47,21 @@ async function Connect(){
             // -32602 - The parameters were invalid
             // -32603- Internal error
         });
+
+        // Баланс
+    
         
     } else {
         window.open("https://metamask.io/download/", "_blank");
     }
 
-    // await window.web3.currentProvider.enable();
-    // web3 = new web3(window.web3.currentProvider);
+}
 
-    // if(!web3.isConnected()) {
-    //     console.error("Not connected");
-    // }
 
-    // console.log(web3);
-
-    // var account = web3.currentProvider.selectedAddress;
-    // console.log(account);
-    // document.getElementById("address").innerHTML = account;
-
+async function disconect()
+{
+    var tokenBalance = await sttcontract.methods.balanceOf(addr).call();
+    tokenBalance = ethers.utils.formatEther(tokenBalance);
+    tokenBalance = (+tokenBalance).toFixed(4);
+    console.log(tokenBalance);
 }
