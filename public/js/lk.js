a09a68d88dc557babc8f75a7bd8fc2463b5805bb
xoheveras <57674283+xoheveras@users.noreply.github.com>
2023-02-16 17:29:40 +0300

    change folders and add profile

--- public/js/lk.js
+++ public/js/lk.js
@@ -6,7 +6,31 @@ Array.prototype.forEach.call( ids, function( el, i ) {
     {
         el.onclick = function () {
             $(".menu-item-selected").toggleClass("menu-item-selected");
+            $(".main-box-activity").toggleClass("main-box-activity");
+
+            let frameID = $(this).attr("number_frame");
+
             $(this).addClass("menu-item-selected");
+            $("#frame-"+frameID).addClass("main-box-activity");
+
         }
     }
 });
+
+function copy()
+{
+    let adressID = document.getElementById("address")
+    let adress = adressID.innerHTML;
+    let myInputAdress = document.getElementById("myInputAdress");
+
+    myInputAdress.value = adress;
+    myInputAdress.select();
+    document.execCommand("copy");
+
+    adressID.innerHTML = "Copied text to clipboard";
+
+    setTimeout(function() {
+        adressID.innerHTML = adress;
+    },700)
+
+}
